9b7caf599c6989741a084782df440ed305f147df
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2020-12-20 13:40:04 +0100

    Refactor

--- texture/main.c
+++ texture/main.c
@@ -25,6 +25,54 @@
 #define main_glattr_tex 2
 
 
+void fill_texture (uint8_t index)
+{
+	int width = 100;
+	int height = 100;
+	uint8_t * data = NULL;
+	unsigned size = width*height*4*sizeof(uint8_t);
+	data = calloc (size, 1);
+	/*
+	for (int x = 0; x < width; ++x)
+	for (int y = 0; y < height; ++y)
+	{
+		uint8_t * p = data + (x*4) + (y*width*4);
+		p[0] = 0;
+		p[1] = 0;
+		p[2] = 0;
+		p[3] = 0;
+		p[index] = 255;
+	}
+	*/
+
+	int x = width/2;
+	int y = height/2;
+	int dx = 0;
+	int dy = 0;
+	for (unsigned n = 0; n < 100; ++n)
+	{
+		x += CLAMP (dx, -2, 2);
+		y += CLAMP (dy, -2, 2);
+		dx += (rand() % 3) - 1;
+		dy += (rand() % 3) - 1;
+		if (x < 0 || x >= width){dx = -dx/2;}
+		if (y < 0 || y >= height){dy = -dy/2;}
+		x = CLAMP (x, 0, width-1);
+		y = CLAMP (y, 0, height-1);
+		int i = (x*4) + (y*width*4);
+		ASSERT (i >= 0);
+		ASSERT (i < size);
+		data[i + index] = 255;
+		//data[i + 1] = 255;
+		//data[i + 2] = 255;
+		//data[i + 3] = 255;
+	}
+
+	glTexImage2D (GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+	glGenerateMipmap (GL_TEXTURE_2D);
+}
+
+
 
 int main (int argc, char * argv[])
 {
@@ -47,7 +95,8 @@ int main (int argc, char * argv[])
 	glBindAttribLocation (glprogram, main_glattr_tex, "tex" );
 	glLinkProgram (glprogram);
 	glUseProgram (glprogram);
-	glUniform1i (glGetUniformLocation (glprogram, "texture1"), 0);
+	GLuint uniform_mvp = glGetUniformLocation (glprogram, "mvp");
+	GLuint uniform_texture1 = glGetUniformLocation (glprogram, "texture1");
 
 	float const p[] =
 	{
@@ -60,12 +109,12 @@ int main (int argc, char * argv[])
 	};
 	float const c[] =
 	{
-	1.0f, 1.0f, 1.0f, 1.0f, // left, bottom
-	1.0f, 1.0f, 1.0f, 1.0f, // right, bottom
-	1.0f, 1.0f, 1.0f, 1.0f, // right, top
-	1.0f, 1.0f, 1.0f, 1.0f, // left, bottom
-	1.0f, 1.0f, 1.0f, 1.0f, // right, top
-	1.0f, 1.0f, 1.0f, 1.0f  // left, top
+	0.0f, 1.0f, 1.0f, 1.0f, // left, bottom
+	0.0f, 1.0f, 1.0f, 1.0f, // right, bottom
+	0.0f, 1.0f, 1.0f, 1.0f, // right, top
+	1.0f, 1.0f, 0.0f, 1.0f, // left, bottom
+	1.0f, 1.0f, 0.0f, 1.0f, // right, top
+	1.0f, 1.0f, 0.0f, 1.0f  // left, top
 	};
 	float const t[] =
 	{
@@ -78,59 +127,61 @@ int main (int argc, char * argv[])
 	};
 
 
-	unsigned int VBOp;
-	unsigned int VBOc;
-	unsigned int VBOt;
-	unsigned int VAO;
+	GLuint vbop;
+	GLuint vboc;
+	GLuint vbot;
+	GLuint vao;
 
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBOp);
-	glGenBuffers(1, &VBOc);
-	glGenBuffers(1, &VBOt);
+	glGenVertexArrays (1, &vao);
+	glGenBuffers (1, &vbop);
+	glGenBuffers (1, &vboc);
+	glGenBuffers (1, &vbot);
 
-	glBindVertexArray(VAO);
+	glBindVertexArray (vao);
 
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBOp);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(p), p, GL_STATIC_DRAW);
+	glBindBuffer (GL_ARRAY_BUFFER, vbop);
+	glBufferData (GL_ARRAY_BUFFER, sizeof(p), p, GL_STATIC_DRAW);
 	glVertexAttribPointer (main_glattr_pos, 4, GL_FLOAT, GL_FALSE, 0, (void*)0);
 	glEnableVertexAttribArray (main_glattr_pos);
 
-	glBindBuffer(GL_ARRAY_BUFFER, VBOc);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(c), c, GL_STATIC_DRAW);
+	glBindBuffer (GL_ARRAY_BUFFER, vboc);
+	glBufferData (GL_ARRAY_BUFFER, sizeof(c), c, GL_STATIC_DRAW);
 	glVertexAttribPointer (main_glattr_col, 4, GL_FLOAT, GL_FALSE, 0, (void*)0);
 	glEnableVertexAttribArray (main_glattr_col);
 
-	glBindBuffer(GL_ARRAY_BUFFER, VBOt);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(t), t, GL_STATIC_DRAW);
+	glBindBuffer (GL_ARRAY_BUFFER, vbot);
+	glBufferData (GL_ARRAY_BUFFER, sizeof(t), t, GL_STATIC_DRAW);
 	glVertexAttribPointer (main_glattr_tex, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
 	glEnableVertexAttribArray (main_glattr_tex);
 
-	unsigned int texture1;
-	glGenTextures(1, &texture1);
-	glBindTexture(GL_TEXTURE_2D, texture1);
+	GLuint textures[2];
+	glGenTextures (2, textures);
+
+
+	glActiveTexture (GL_TEXTURE0 + 0);
+	glBindTexture (GL_TEXTURE_2D, textures[0]);
 	// set the texture wrapping parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+	glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	// set texture filtering parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	fill_texture (0);
 
-	{
-		int width = 100;
-		int height = 100;
-		uint8_t * data = NULL;
-		unsigned size = width*height*4*sizeof(uint8_t);
-		data = malloc (size);
-		for (unsigned i = 0; i < size; ++i)
-		{
-			data[i] = rand();
-		}
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
+	glActiveTexture (GL_TEXTURE0 + 1);
+	glBindTexture (GL_TEXTURE_2D, textures[1]);
+	// set the texture wrapping parameters
+	glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+	glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// set texture filtering parameters
+	glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	fill_texture (2);
 
-	GLuint uniform_mvp = glGetUniformLocation (glprogram, "mvp");
+
+
+
+
 	struct csc_gcam gcam;
 	csc_gcam_init (&gcam);
 	v4f32_set_xyzw (gcam.p, 0.0f, 0.0f, -4.0f, 1.0f);
@@ -156,11 +207,22 @@ int main (int argc, char * argv[])
 		glClearColor (0.2f, 0.3f, 0.3f, 1.0f);
 		glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		glUseProgram (glprogram);
-		glActiveTexture (GL_TEXTURE0);
-		glBindTexture (GL_TEXTURE_2D, texture1);
-		glUniformMatrix4fv (uniform_mvp, 1, GL_FALSE, (const GLfloat *) gcam.mvp);
-		glBindVertexArray (VAO);
+
+		float mvp[4*4];
+		float model[4*4];
+
+		m4f32_identity (model);
+		m4f32_translation_xyz (model, 1.0f, 1.0f, 0.0f);
+		m4f32_mul (mvp, gcam.mvp, model);
+		glUniformMatrix4fv (uniform_mvp, 1, GL_FALSE, (const GLfloat *) mvp);
+		glUniform1i (uniform_texture1, 0);
+		glDrawArrays (GL_TRIANGLES, 0, 6);
+
+		m4f32_identity (model);
+		m4f32_translation_xyz (model, -1.0f, -1.0f, 0.0f);
+		m4f32_mul (mvp, gcam.mvp, model);
+		glUniformMatrix4fv (uniform_mvp, 1, GL_FALSE, (const GLfloat *) mvp);
+		glUniform1i (uniform_texture1, 1);
 		glDrawArrays (GL_TRIANGLES, 0, 6);
 
 		SDL_Delay (10);
